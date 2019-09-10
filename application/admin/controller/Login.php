83001197897f83de0d5c3aa1cd6768a637daf815
need <yahoushan@kmindo.com>
2019-09-10 16:29:36 +0800

    no message

--- application/admin/controller/Login.php
+++ application/admin/controller/Login.php
@@ -6,12 +6,8 @@ use think\Db;
 class Login extends AdminBaseController
 {
     public function index()
-<<<<<<< HEAD
     {   
-        $find = Db::name('login')->where('id',1)->find();print_r($find);die;
-=======
-    {
->>>>>>> cf5537026b1ad47e79c9af2dc13e0cbb3c6d8b4c
+        
         return $this->adminTpl();
     }
 }
