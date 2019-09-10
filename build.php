fdda3fa6f656778664d381400ad11b4a5bc7d014
欧阳辉 <982804907@qq.com>
2019-09-10 11:18:25 +0800

    测试build.php

--- build.php
+++ build.php
@@ -21,6 +21,13 @@ return [
         'model'      => ['User', 'UserType'],
         'view'       => ['index/index'],
     ],
+    'demo'     => [
+        '__file__'   => ['common.php'],
+        '__dir__'    => ['behavior', 'controller', 'model', 'view'],
+        'controller' => ['Index', 'Test', 'UserType'],
+        'model'      => ['User', 'UserType'],
+        'view'       => ['index/index'],
+    ],
 
     // 其他更多的模块定义
 ];
