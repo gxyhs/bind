0adb31f3f3aff40ffe7ed53cebb3c2ab2a61548c
need <yahoushan@kmindo.com>
2019-09-10 14:34:13 +0800

    no message

--- application/admin/controller/Login.php
+++ application/admin/controller/Login.php
@@ -1,10 +1,12 @@
 <?php
 namespace app\admin\controller;
 use app\Common\Controller\AdminBaseController;
+use think\Db;
+
 class Login extends AdminBaseController
 {
     public function index()
-    {
+    {   
         return $this->adminTpl();
     }
 }
