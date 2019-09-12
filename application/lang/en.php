1b0b922aa5d0dfd588b164530356da203560308c
欧阳辉 <982804907@qq.com>
2019-09-12 17:30:46 +0800

    PHP execl

--- application/lang/en.php
+++ application/lang/en.php
@@ -4,8 +4,12 @@ return [
     'navigation' => 'Navigation',
     'pretest' => 'Pre-test outbound call',
     'upload_call_case' => 'Upload call case',
+<<<<<<< Updated upstream
 
     'logout' => 'Log Out',
+=======
+    'role_manage' => 'role manage',
+>>>>>>> Stashed changes
 ];
 
 ?>
--- application/lang/zh-cn.php
+++ application/lang/zh-cn.php
@@ -4,8 +4,12 @@ return [
     'navigation' => '导航',
     'pretest' => '预测试外呼后台',
     'upload_call_case' => '上传呼叫案例',
+<<<<<<< Updated upstream
 
     'logout' => '退出登录'
+=======
+    'role_manage' => '角色管理',
+>>>>>>> Stashed changes
 ];
 
 ?>
