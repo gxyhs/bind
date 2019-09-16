985282ceddf350839a5b440fd95c3ff33369f2f9
ouyanghui-git <982804907@qq.com>
2019-09-16 18:50:57 +0800

    导入导出功能

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -33,7 +33,7 @@ use app\Common\Model\AdminUserModel;use think\facade\App;class Index extends Adm
                 cookie('think_var', 'ZH-CN');
         }
     }
-public function change_password(){
+    public function change_password(){
         if(IS_POST){
             $original = md5(trim($_POST['original_password']));
             $uid = session('admin_uid');
@@ -62,125 +62,31 @@ public function change_password(){
         }else{
             return json(['status'=>0]);
         }
-    }	public function pull_out(){
-        $where = new Where();
-        $result = $this->model->where($where)->order('id desc')->select();
-        // 导出到excel
-        foreach ($result as $k=>&$v) {
-            $v['add_time'] = date('Y-m-d H:i',$v['add_time']);
-            switch ($v['status']) {
-                case 0:
-                    $v['status'] = '未处理';
-                    break;
-                case 1:
-                    $v['status'] = '已同意申请';
-                    break;
-                case 2:
-                    $v['status'] = '已拒绝申请';
-                    break;
-            }
-        }
-        $xlsCell  = array(
-            array('id','ID'),
-            array('name','姓名'),
-            array('telphone','手机号'),
-            array('add_time','领取时间'),
-            array('status','状态')
-        );
-        $xlsName = date('Y-m-d').'申请加入名单';
-        Func::exportExcel($xlsName, $xlsCell, $result);
     }
 
-    public static function exportExcel($expTitle, $expCellName, $expTableData, $topData) {
-        $xlsTitle = iconv('utf-8', 'gb2312', $expTitle);//文件名称
-        $fileName = $xlsTitle;//or $xlsTitle 文件名称可根据自己情况设定
-        $cellNum = count($expCellName);
-        $dataNum = count($expTableData);
-        $topNum  = count($topData);
-
-        $objPHPExcel = new \PHPExcel();
-        $cellName = array('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'AA', 'AB', 'AC', 'AD', 'AE', 'AF', 'AG', 'AH', 'AI', 'AJ', 'AK', 'AL', 'AM', 'AN', 'AO', 'AP', 'AQ', 'AR', 'AS', 'AT', 'AU', 'AV', 'AW', 'AX', 'AY', 'AZ');
-
-        $objPHPExcel->getActiveSheet(0)->mergeCells('A1:' . $cellName[$cellNum - 1] . '1');//合并单元格
-        $objPHPExcel->setActiveSheetIndex(0)->setCellValue('A1', $expTitle);
-
-        for ($i = 0; $i < count($topData); $i++) {
-            for ($j = 0; $j < count($topData[$i]); $j++) {
-                $objPHPExcel->setActiveSheetIndex(0)->setCellValue($cellName[$j] . ($i + 2), $topData[$i][$j]);
-            }
-        }
-
-        for ($i = 0; $i < $cellNum; $i++) {
-            $objPHPExcel->setActiveSheetIndex(0)->setCellValue($cellName[$i] . ($topNum + 2), $expCellName[$i][1]);
-        }
-        // Miscellaneous glyphs, UTF-8
-        for ($i = 0; $i < $dataNum; $i++) {
-            for ($j = 0; $j < $cellNum; $j++) {
-                if ($expCellName[$j][0] == 'account_type') {
-                    if ($expTableData[$i][$expCellName[$j][0]] == 0) {
-                        $objPHPExcel->getActiveSheet(0)->setCellValue($cellName[$j] . ($i + $topNum + 3), '餐饮');
-                    } else {
-                        $objPHPExcel->getActiveSheet(0)->setCellValue($cellName[$j] . ($i + $topNum + 3), '果蔬');
-                    }
-                } else {
-                    $objPHPExcel->getActiveSheet(0)->setCellValue($cellName[$j] . ($i + $topNum + 3), $expTableData[$i][$expCellName[$j][0]]);
-                }
-            }
-        }
-
-        header('pragma:public');
-        header('Content-type:application/vnd.ms-excel;charset=utf-8;name="' . $xlsTitle . '.xls"');
-        header("Content-Disposition:attachment;filename=$fileName.xls");//attachment新窗口打印inline本窗口打印
-        $objWriter = \PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel5');
-        $objWriter->save('php://output');
+    /**
+     * 测试导出
+     */
+    public function demo_out(){
+        $data = Db::table('tp_admin_user')->field('id,sex,email,user_name')->select();
+        $head = ['id','sex','emal','user_naem'];
+        $name = 'user';
+        return leading_out($data,$head,$name);
         exit;
     }
 
-    public function pull_in(){
-        $rootPath = App::getRootPath();
-        require $rootPath.'extend/PHPExcel/PHPExcel.php';
-        // dump($file);die;
-        // 移动到框架应用根目录/public/uploads/ 目录下
-//        $info = $file->move(ROOT_PATH . 'public' . DS . 'uploads' . DS . 'excel');
-        $file = $rootPath.'/upload/123.xlsx';
-        if ($file) {
-            //获取文件所在目录名
-//            $path = ROOT_PATH . 'public' . DS . 'uploads' . DS . 'excel/' . $info->getSaveName();
-            $filename = '123.xlsx';
-            //获取文件名后缀
-            $suf = substr($filename, strrpos($filename, '.'));
-            if ($suf == '.xlsx') {
-                $objReader = new \PHPExcel_Reader_Excel2007();
-            } else if($suf == '.xls'){
-                $objReader = new \PHPExcel_Reader_Excel5();
-            }else{
-                return '文件类型错误';
+    /**
+     * 测试导入
+     */
+    public function demo_in(){
+        if($_FILES){
+            $file = request()->file('excel');
+            $res = leading_in($file);
+            if(is_array($res)){
+                $this->success('成功',url('Index/index'));
             }
-            $objPHPExcel = $objReader->load($file, $encode = 'utf-8'); //获取excel文件
-            $sheet = $objPHPExcel->getSheet(0); //激活当前的表
-            $highestRow = $sheet->getHighestRow(); // 取得总行数
-            $highestColumn = $sheet->getHighestColumn(); // 取得总列数
-            $a = 0;
-            //将表格里面的数据循环到数组中
-            for ($i = 2; $i <= $highestRow; $i++) {
-                //*为什么$i=2? (因为Excel表格第一行应该是姓名，年龄，班级，从第二行开始，才是我们要的数据。)
-                $data[$a]['id'] = $objPHPExcel->getActiveSheet()->getCell("A" . $i)->getValue(); //姓名
-                $data[$a]['user_name'] = $objPHPExcel->getActiveSheet()->getCell("B" . $i)->getValue(); //年龄
-                $data[$a]['password'] = $objPHPExcel->getActiveSheet()->getCell("C" . $i)->getValue(); //班级
-                // 这里的数据根据自己表格里面有多少个字段自行决定
-                $a++;
-            }
-
-            //往数据库添加数据
-            // $res = Db::name('student')->insertAll($data);
-            // if($res){
-            //         $this->success('操作成功！');
-            // }else{
-            //         $this->error('操作失败！');
-            //    }
-            dump($data);die;
-        } else {
-            // 上传失败获取错误信息
-            $this->error($file->getError());
+        }else{
+            return $this->fetch();
         }
-    }}}
+    }
+}
--- application/common.php
+++ application/common.php
@@ -8,5 +8,171 @@
 // +----------------------------------------------------------------------
 // | Author: 流年 <liu21st@gmail.com>
 // +----------------------------------------------------------------------
-
+$rootPath = App::getRootPath();
+require $rootPath.'extend/PHPExcel/PHPExcel.php';
 // 应用公共文件
+/**
+ * @param $data 导出的数据
+ * @param $head 第一行的标题
+ * @param $name 文件名
+ * @throws PHPExcel_Exception
+ * @throws PHPExcel_Reader_Exception
+ * @throws PHPExcel_Writer_Exception
+ */
+ function leading_out($data,$head,$name){
+
+    $num = 1;
+    $letter_begin = 65;
+    date_default_timezone_set("Asia/Shanghai");
+    $objPHPExcel = new \PHPExcel();
+    $objPHPExcel->getProperties()->setCreator("admin")  // 创建者
+    ->setLastModifiedBy("admin")                    // 最后修改者
+    ->setTitle($name)                               // 标题
+    ->setSubject($name)                             // 主题
+    ->setDescription($name)                         // 描述
+    ->setKeywords("excel")                          // 关键字
+    ->setCategory("result file");                   // 种类
+
+    $A = $letter_begin;
+    $format = [];
+    for($i = 0;$i<count($head);$i++){
+        $f=intval($i/26)+64;
+        $s=($i%26)+$A;
+        if(is_array($head[$i])){
+            $element = $head[$i][0];
+            $format[$i] = $head[$i][1];
+        }else{
+            $element = $head[$i];
+        }
+        if($f>=$A){
+            $objPHPExcel->setActiveSheetIndex(0)
+                ->setCellValue(chr($f).chr($s).$num,$element);
+        }else{
+            $objPHPExcel->setActiveSheetIndex(0)
+                ->setCellValue(chr($s).$num,$element);
+        }
+
+    }
+    foreach($data as $k => $v){
+        $A = $letter_begin;
+        $num += 1;
+        $count = 0;
+        foreach($v as $val){
+            $f=intval($count/26)+64;
+            $s=($count%26)+$A;
+            if(isset($format[$count])){
+                switch($format[$count]){
+                    case 'string':
+                        if($f>=$A){
+                            $objPHPExcel->setActiveSheetIndex(0)
+                                ->setCellValueExplicit(chr($f).chr($s).$num,$val, PHPExcel_Cell_DataType::TYPE_STRING);
+                        }else{
+                            $objPHPExcel->setActiveSheetIndex(0)
+                                ->setCellValueExplicit(chr($s).$num,$val, PHPExcel_Cell_DataType::TYPE_STRING);
+                        }
+                        break;
+                    case 'numeric':
+                        if($f>=$A){
+                            $objPHPExcel->setActiveSheetIndex(0)
+                                ->setCellValueExplicit(chr($f).chr($s).$num,$val, PHPExcel_Cell_DataType::TYPE_NUMERIC);
+                        }else{
+                            $objPHPExcel->setActiveSheetIndex(0)
+                                ->setCellValueExplicit(chr($s).$num,$val, PHPExcel_Cell_DataType::TYPE_NUMERIC);
+                        }
+                        break;
+                    case 'date':
+                        if($f>=$A){
+                            if(empty($val)){
+                                $objPHPExcel->setActiveSheetIndex(0)->setCellValue(chr($f).chr($s).$num,$val);
+                            }else{
+                                $objPHPExcel->setActiveSheetIndex(0)->setCellValue(chr($f).chr($s).$num, PHPExcel_Shared_Date::stringToExcel($val));
+                                $objPHPExcel->getActiveSheet()->getStyle(chr($f).chr($s).$num)->getNumberFormat()->setFormatCode(PHPExcel_Style_NumberFormat::FORMAT_DATE_YYYYMMDD2);
+                            }
+                        }else{
+                            if(empty($val)){
+                                $objPHPExcel->setActiveSheetIndex(0)->setCellValue(chr($s).$num,$val);
+                            }else{
+                                $objPHPExcel->setActiveSheetIndex(0)->setCellValue(chr($s).$num,PHPExcel_Shared_Date::stringToExcel($val));
+                                $objPHPExcel->getActiveSheet()->getStyle(chr($s).$num)->getNumberFormat()->setFormatCode(PHPExcel_Style_NumberFormat::FORMAT_DATE_YYYYMMDD2);
+                            }
+                        }
+                        break;
+                }
+
+            }else{
+                if($f>=$A){
+                    $objPHPExcel->setActiveSheetIndex(0)
+                        ->setCellValueExplicit(chr($f).chr($s).$num,$val, \PHPExcel_Cell_DataType::TYPE_STRING);
+                }else{
+                    $objPHPExcel->setActiveSheetIndex(0)
+                        ->setCellValueExplicit(chr($s).$num,$val, \PHPExcel_Cell_DataType::TYPE_STRING);
+                }
+            }
+            $count++;
+        }
+    }
+
+    $objPHPExcel->getActiveSheet()->setTitle($name);
+    $objPHPExcel->setActiveSheetIndex(0);
+    ob_end_clean();
+    // Redirect output to a client’s web browser (Excel5)
+    header('Content-Type: application/vnd.ms-excel');
+    header('Content-Disposition: attachment;filename="'.$name.'.xlsx"');
+    header('Cache-Control: max-age=0');
+    // If you're serving to IE 9, then the following may be needed
+    header('Cache-Control: max-age=1');
+
+    // If you're serving to IE over SSL, then the following may be needed
+    header ('Expires: Mon, 26 Jul 1997 05:00:00 GMT'); // Date in the past
+    header ('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT'); // always modified
+    header ('Cache-Control: cache, must-revalidate'); // HTTP/1.1
+    header ('Pragma: public'); // HTTP/1.0
+
+    $objWriter = \PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');
+    $objWriter->save('php://output');
+    exit;
+}
+
+/**
+ * 导入execl文件
+ * @return string
+ * @throws \PHPExcel_Exception
+ * @throws \PHPExcel_Reader_Exception
+ */
+ function leading_in($file){
+
+    // 移动到框架应用根目录/public/uploads/ 目录下
+     $info = $file->validate(['size'=>1048576,'ext'=>'xls,xlsx'])->move( './upload');
+    if ($info) {
+        $fileName = $info->getSaveName();
+        //获取文件路径
+        $filePath = App::getRootPath().'public'.DIRECTORY_SEPARATOR.'upload'.DIRECTORY_SEPARATOR.$fileName;
+        //获取文件后缀
+        $suf = $info->getExtension();
+        if ($suf == 'xlsx') {
+            $objReader = new \PHPExcel_Reader_Excel2007();
+        } else if($suf == 'xls'){
+            $objReader = new \PHPExcel_Reader_Excel5();
+        }else{
+            return '文件类型错误';
+        }
+        $objPHPExcel = $objReader->load($filePath, $encode = 'utf-8'); //获取excel文件
+        $sheet = $objPHPExcel->getSheet(0); //激活当前的表
+        $highestRow = $sheet->getHighestRow(); // 取得总行数
+        $highestColumn = $sheet->getHighestColumn(); // 取得总列数
+        $a = 0;
+        //将表格里面的数据循环到数组中
+        for ($i = 2; $i <= $highestRow; $i++) {
+            //*为什么$i=2? (因为Excel表格第一行应该是姓名，年龄，班级，从第二行开始，才是我们要的数据。)
+            $data[$a]['id'] = $objPHPExcel->getActiveSheet()->getCell("A" . $i)->getValue(); //姓名
+            $data[$a]['user_name'] = $objPHPExcel->getActiveSheet()->getCell("B" . $i)->getValue(); //年龄
+            $data[$a]['password'] = $objPHPExcel->getActiveSheet()->getCell("C" . $i)->getValue(); //班级
+            // 这里的数据根据自己表格里面有多少个字段自行决定
+            $a++;
+        }
+        return $data;
+    } else {
+        // 上传失败获取错误信息
+        $this->error($file->getError());
+    }
+}
