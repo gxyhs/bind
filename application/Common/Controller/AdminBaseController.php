98731ec5d96551b5a0030c34d7961c3993c2ad59
need <yahoushan@kmindo.com>
2019-09-16 19:19:21 +0800

    no message

--- application/Common/Controller/AdminBaseController.php
+++ application/Common/Controller/AdminBaseController.php
@@ -44,7 +44,47 @@ class AdminBaseController extends BaseController {
     public function _empty() {
         return $this->jump404();
     }
-    
-
-
+    public function get_paging_info(){
+        if(empty($_POST['aoData'])) return [];
+        $res = $_POST['aoData'];
+        $iDisplayStart = 0; // 起始索引
+        $iDisplayLength = 0;//分页长度
+        $jsonarray= json_decode($res);
+        foreach($jsonarray as $value){
+            if($value->name=="sEcho"){
+                $sEcho=$value->value;
+            }
+            if($value->name=="iDisplayStart"){
+                $iDisplayStart=$value->value;
+            }
+            if($value->name=="iDisplayLength"){
+                $iDisplayLength=$value->value;
+            }
+        }
+        return [
+            'page_echo' =>$sEcho,
+            'page_start'=>$iDisplayStart,
+            'page_length'=>$iDisplayLength,
+        ];
+    }
+    public function show_paging_info($echo,$count,$ary){
+        $ary = json_decode($ary);
+        $return_ary = [];
+        foreach($ary as $val){
+            $temp = array_values($val);
+            array_push($return_ary,$temp);
+        }
+        $json_data = array ('sEcho'=>$echo,'iTotalRecords'=>$count,'iTotalDisplayRecords'=>$count,'aaData'=>$return_ary);
+        return $json_data;
+    }
+    public function object_array($array) {  
+        if(is_object($array)) {  
+            $array = (array)$array;  
+         } if(is_array($array)) {  
+             foreach($array as $key=>$value) {  
+                 $array[$key] = $this->object_array($value);  
+            }  
+         }  
+         return $array;  
+    }
 }
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,10 +6,15 @@ namespace Composer\Autoload;
 
 class ComposerStaticInita6bbb41bd153515019bad437e6037a49
 {
+    public static $files = array (
+        '1cfd2761b63b0a29ed23657ea394cb2d' => __DIR__ . '/..' . '/topthink/think-captcha/src/helper.php',
+    );
+
     public static $prefixLengthsPsr4 = array (
         't' => 
         array (
             'think\\composer\\' => 15,
+            'think\\captcha\\' => 14,
         ),
         'a' => 
         array (
@@ -21,6 +26,10 @@ class ComposerStaticInita6bbb41bd153515019bad437e6037a49
         'think\\composer\\' => 
         array (
             0 => __DIR__ . '/..' . '/topthink/think-installer/src',
+        ),
+        'think\\captcha\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/topthink/think-captcha/src',
         ),
         'app\\' => 
         array (
