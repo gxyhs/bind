a7d092884c65efe1b2324888483e43b9baf439e6
ouyanghui-git <982804907@qq.com>
2019-09-17 18:15:01 +0800

    11

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -17,6 +17,25 @@ class Index extends AdminBaseController
 	}
     public function index()
     {
+        if($_POST || $_GET){
+            $table = new AdminUserModel();
+            $info = $this->get_paging_info();
+            if(count($info)){
+                $length = $info['page_length'];
+                $start = $info['page_start'];
+                if(!empty(input('search'))){
+                    $list = $table->field('id,user_name,email,create_time')->whereOr([['user_name','like',"%".input('search')."%"]])->whereOr([['email','like',"%".input('search')."%"]])->limit($start,$length)->select();
+                }else{
+                    $list = $table->field('id,user_name,email,create_time')->limit($start,$length)->select();
+                }
+                $list = $this->object_array($list);
+                $this->assign('list',$list);
+                $count = $table->count();
+                $data =  $this->show_paging_info($info['page_echo'],$count,$list);
+                return $data;
+            }
+        }
+        $this->assign('search',input('search'));
         return $this->fetch();
     }
     public function lang(){
@@ -86,11 +105,10 @@ class Index extends AdminBaseController
             $file = request()->file('excel');
             $res = leading_in($file);
             if(is_array($res)){
-
                 $this->success('导入成功',url('Index/index'));
             }
-        }else{
-            return $this->fetch();
         }
+        return $this->fetch();
+
     }
 }
