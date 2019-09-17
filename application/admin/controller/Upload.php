4985c9584de27327fe9ad30990f465c3219a399c
need <yahoushan@kmindo.com>
2019-09-17 15:31:23 +0800

    no message

--- application/admin/controller/Upload.php
+++ application/admin/controller/Upload.php
@@ -6,18 +6,26 @@ class Upload extends AdminBaseController
 {   
     public function list()
     {   
-        if($_POST){
+        
+        if($_POST || $_GET){
             $table = new AdminUserModel();
             $info = $this->get_paging_info();
-            $length = $info['page_length'];
-            $start = $info['page_start'];
-            $list = $table->field('id,user_name,create_time')->limit($start,$length)->select();
-            $list = $this->object_array($list);
-            $count = $table->count();
-            $data =  $this->show_paging_info($info['page_echo'],$count,$list);
-            return $data;
+            if(count($info)){
+                $length = $info['page_length'];
+                $start = $info['page_start'];
+                if(!empty(input('search'))){
+                    $list = $table->field('id,user_name,email,create_time')->whereOr([['user_name','like',"%".input('search')."%"]])->whereOr([['email','like',"%".input('search')."%"]])->limit($start,$length)->select();
+                }else{
+                    $list = $table->field('id,user_name,email,create_time')->limit($start,$length)->select();
+                }
+                $list = $this->object_array($list);
+                $this->assign('list',$list);
+                $count = $table->count();
+                $data =  $this->show_paging_info($info['page_echo'],$count,$list);
+                return $data;
+            }
         }
-        
+        $this->assign('search',input('search'));
         return $this->fetch();
     }
    
