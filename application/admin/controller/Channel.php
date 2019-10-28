2e905937a6bf119cd0c918a64e2ac18a607cd9e7
yinyuezazhishe <1642822917@qq.com>
2019-10-28 16:58:50 +0800

    模板下载修改

--- application/admin/controller/Channel.php
+++ application/admin/controller/Channel.php
@@ -500,26 +500,4 @@ class Channel extends ChannelBaseController
         $this->assign('status',$this->status);
         return $this->fetch();
     }
-    public function downloadTemplate()
-    {
-        $file = Env::get('root_path').'public/static/call_case.xlsx';
-        if(!file_exists($file)){
-            return $this->error('文件不存在');
-        }else{
-            // 打开文件
-            $file_operation = fopen($file, 'r');
-            // 输入文件标签
-            Header('Content-type: application/octet-stream');
-            Header('Accept-Ranges: bytes');
-            Header('Accept-Length:'.filesize($file));
-            Header('Content-Disposition: attachment;filename=call_case.xlsx');
-            ob_clean();
-            flush();
-            //输出文件内容
-            //读取文件内容并直接输出到浏览器
-            echo fread($file_operation, filesize($file));
-            fclose($file_operation);
-            exit();
-        }
-    }
 }
--- application/admin/controller/Login.php
+++ application/admin/controller/Login.php
@@ -4,6 +4,7 @@ use think\Controller;
 use think\Validate;
 use think\captcha\Captcha;
 use app\Common\Model\ChannelUserModel;
+use think\facade\Env;
 
 /**
  * 渠道用户登录
@@ -51,6 +52,28 @@ class Login extends Controller
     function verify(){
         $captcha = new Captcha();
         return $captcha->entry();
+    }
+    public function downloadTemplate()
+    {
+        $file = Env::get('root_path').'public/static/call_case.xlsx';
+        if(!file_exists($file)){
+            return $this->error('文件不存在');
+        }else{
+            // 打开文件
+            $file_operation = fopen($file, 'r');
+            // 输入文件标签
+            Header('Content-type: application/octet-stream');
+            Header('Accept-Ranges: bytes');
+            Header('Accept-Length:'.filesize($file));
+            Header('Content-Disposition: attachment;filename=call_case.xlsx');
+            ob_clean();
+            flush();
+            //输出文件内容
+            //读取文件内容并直接输出到浏览器
+            echo fread($file_operation, filesize($file));
+            fclose($file_operation);
+            exit();
+        }
     }
     public function logout() {
         session('is_login',NUll);
